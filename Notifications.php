56bd7dc50718c75f379180a0a6967cc7cdbc0659
stephancarpin <viviancarpin@gmail.com>
2021-05-20 12:11:50 +0000

    add changes

---
+++ Notifications.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephancarpin
+ * Date: 20/05/2021
+ * Time: 09:46
+ */
+
+namespace App\Classes;
+
+
+class Notifications
+{
+    public function __construct() {
+        return "construct function was initialized for Notification app.";
+    }
+
+    public function create() {
+        // create notification
+        // send email
+        // return output
+
+        return "create notification";
+
+    }
+}
